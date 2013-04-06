1342e882830bcdec05a16660e0fb9d6fee6f26ca
Olli Vanhoja <olli.vanhoja@gmail.com>
2013-04-06 22:10:27 +0300

    Fixed MainActivity & Enemy

--- HomokaasuTheGame/src/com/example/homokaasuthegame/Enemy.java
+++ HomokaasuTheGame/src/com/example/homokaasuthegame/Enemy.java
@@ -1,58 +1,49 @@
 package com.example.homokaasuthegame;
 
-import org.andengine.entity.Entity;
-import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.entity.sprite.Sprite;
-import org.andengine.entity.sprite.vbo.ITiledSpriteVertexBufferObject;
 import org.andengine.extension.physics.box2d.PhysicsConnector;
+import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.opengl.texture.region.ITextureRegion;
-import org.andengine.opengl.texture.region.ITiledTextureRegion;
-import org.andengine.opengl.texture.region.TiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
-import android.util.DisplayMetrics;
-
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Fixture;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 
 
 public class Enemy extends Sprite {
 
-    private static BodyDef bd = new BodyDef();
-	private static FixtureDef fd = new FixtureDef();
-	
+    protected static BodyDef bd;
+	protected static FixtureDef fd;
+
+	static {
+	    bd = new BodyDef();
+	    fd = new FixtureDef();
+	}
+
 	public Enemy(float pX, float pY, float pWidth, float pHeight,
 			ITextureRegion pTextureRegion,
 			VertexBufferObjectManager vertexBufferObjectManager) {
 		super(0, 0, 0, 0, pTextureRegion,
 				vertexBufferObjectManager);
-		
+
 		bd.type = BodyType.DynamicBody;
 		bd.active = true;
-		
-		bd.position.set(2, 2);
-		
+		bd.position.set(1f, 2f);
+
 		fd.density = 1.0f;
 		fd.friction = 0.1f;
 		fd.restitution = 0.01f;
-		Body b = MainActivity.world.createBody(bd);	
-		b.createFixture(fd);
-		
-		MainActivity.physicsWorld.registerPhysicsConnector(new PhysicsConnector(this,
-                b, true, false));
-		
-		
-		/*Enemy n = new Enemy(world.createBody(bd));
-		n.getBody().createFixture(fd);*/
+
+		//Body body = MainActivity.physicsWorld.createBody(bd);
+		Body body = PhysicsFactory.createCircleBody(MainActivity.physicsWorld,
+		        this, BodyType.DynamicBody, fd);
+		//body.createFixture(fd);
+
+		MainActivity.physicsWorld.registerPhysicsConnector(
+		        new PhysicsConnector(this, body, true, false));
+		MainActivity.mainScene.attachChild(this);
 	}
-	//Body and Fixture definitions for things
-	
-	
-	
-	
+
 }
