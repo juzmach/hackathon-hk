da8dc670599e3b3d889945f1b478e93b217b87b7
Kalle Viiri <kviiri@cs.helsinki.fi>
2013-04-06 15:41:54 +0300

    more stuff to addEnemy

--- HomokaasuTheGame/src/com/example/homokaasuthegame/State.java
+++ HomokaasuTheGame/src/com/example/homokaasuthegame/State.java
@@ -1,14 +1,26 @@
 package com.example.homokaasuthegame;
 
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class State {
 	private World world;
+	private BodyDef bd = new BodyDef();
+	private FixtureDef fd = new FixtureDef();
 	
 	public void addEnemy() {
-		BodyDef def = new BodyDef();
-		def.type = BodyType.DynamicBody;
+		bd.type = BodyType.DynamicBody;
+		bd.active = true;
+		
+		bd.position.set(0, 0);
+		
+		fd.density = 1.0f;
+		fd.friction = 0.1f;
+		fd.restitution = 0.01f;
+		
+		world.createBody(bd).createFixture(fd);
 	}
 }
