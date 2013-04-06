0927ffa185e8285d72b85db5d1c2e8cd05dddb74
Kalle Viiri <kviiri@cs.helsinki.fi>
2013-04-06 18:54:24 +0300

    pushing stuff that breaks compile :)

--- HomokaasuTheGame/src/com/example/homokaasuthegame/Enemy.java
+++ HomokaasuTheGame/src/com/example/homokaasuthegame/Enemy.java
@@ -2,6 +2,10 @@ package com.example.homokaasuthegame;
 
 
 import org.andengine.entity.Entity;
+import org.andengine.entity.sprite.AnimatedSprite;
+import org.andengine.opengl.texture.region.ITiledTextureRegion;
+import org.andengine.opengl.texture.region.TiledTextureRegion;
+import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
 import android.util.DisplayMetrics;
 
@@ -10,19 +14,31 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.World;
 
 
-public class Enemy extends Entity {
+public class Enemy extends AnimatedSprite {
+	public Enemy(float pX, float pY, float pWidth, float pHeight,
+			ITiledTextureRegion pTiledTextureRegion,
+			VertexBufferObjectManager pVertexBufferObjectManager) {
+		super(pX, pY, pWidth, pHeight, pTiledTextureRegion, pVertexBufferObjectManager);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Enemy(float pX, float pY, float pWidth, float pHeight,
+			ITiledTextureRegion pTiledTextureRegion,
+			VertexBufferObjectManager pVertexBufferObjectManager, Body b) {
+		super(pX, pY, pWidth, pHeight, pTiledTextureRegion, pVertexBufferObjectManager);
+		// TODO Auto-generated constructor stub
+	}
+
+
+
 	private Body body;
 	
-	public Enemy(Body body) {
-		this.body = body;
-	}
 	
-	public Body getBody() {
-		return body;
-	}
 	
 	@Override
 	protected void onManagedUpdate(final float pSecondsElapsed) {
 		super.onManagedUpdate(pSecondsElapsed);
 	}
+	
+	
 }
