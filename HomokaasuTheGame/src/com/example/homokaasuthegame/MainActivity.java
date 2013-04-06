b279d2ff0f8cfd9cdcdf98dbe60ff2575e357d6b
Olli Vanhoja <olli.vanhoja@gmail.com>
2013-04-06 18:02:25 +0300

    It Compiles \o/

--- HomokaasuTheGame/src/com/example/homokaasuthegame/MainActivity.java
+++ HomokaasuTheGame/src/com/example/homokaasuthegame/MainActivity.java
@@ -8,15 +8,19 @@ import org.andengine.entity.primitive.Rectangle;
 import org.andengine.entity.scene.Scene;
 import org.andengine.entity.scene.background.Background;
 import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.text.Text;
 import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
 import org.andengine.extension.physics.box2d.PhysicsWorld;
+import org.andengine.opengl.font.Font;
+import org.andengine.opengl.font.FontFactory;
+import org.andengine.opengl.texture.TextureOptions;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
 import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.ui.activity.BaseGameActivity;
-import org.andengine.util.color.Color;
 
+import android.graphics.Color;
 import android.hardware.SensorManager;
 
 import com.badlogic.gdx.math.Vector2;
@@ -26,11 +30,15 @@ import com.badlogic.gdx.physics.box2d.FixtureDef;
 
 public class MainActivity extends BaseGameActivity {
     Scene scene;
-    protected static final int CAMERA_WIDTH = 800;
-    protected static final int CAMERA_HEIGHT = 480;
+    protected static final int CAMERA_WIDTH = 1024;
+    protected static final int CAMERA_HEIGHT = 600;
     BitmapTextureAtlas playerTexture;
     ITextureRegion playerTexureRegion;
     PhysicsWorld physicsWorld;
+    private BitmapTextureAtlas mFontTexture;
+    private Font mFont;
+
+    private Text text;
 
     @Override
     public EngineOptions onCreateEngineOptions() {
@@ -50,8 +58,25 @@ public class MainActivity extends BaseGameActivity {
     public void onCreateResources(
             OnCreateResourcesCallback pOnCreateResourcesCallback)
             throws Exception {
+        loadFonts();
         loadGfx();
         pOnCreateResourcesCallback.onCreateResourcesFinished();
+    }
+
+    private void loadFonts() {
+        /* Load the font we are going to use. */
+        FontFactory.setAssetBasePath("fonts/");
+        this.mFontTexture = new BitmapTextureAtlas(this.getTextureManager(),
+                1024, 1024,
+                TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+        this.mFont = FontFactory.createFromAsset(
+                this.getFontManager(),
+                mFontTexture,
+                this.getAssets(),
+                "BistroSketch.ttf",
+                80.0f, true, Color.WHITE);
+        this.mEngine.getTextureManager().loadTexture(this.mFontTexture);
+        this.getFontManager().loadFont(this.mFont);
     }
 
     private void loadGfx() {
@@ -59,7 +84,8 @@ public class MainActivity extends BaseGameActivity {
        BitmapTextureAtlasTextureRegionFactory.setAssetBasePath("gfx/");
        // width and height power of 2^x
        playerTexture = new BitmapTextureAtlas(getTextureManager(), 64, 64);
-       playerTexureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(playerTexture, this, "player.png", 0, 0);
+       playerTexureRegion = BitmapTextureAtlasTextureRegionFactory.createFromAsset(
+               playerTexture, this, "player.png", 0, 0);
        playerTexture.load();
     }
 
@@ -81,7 +107,7 @@ public class MainActivity extends BaseGameActivity {
         FixtureDef WALL_FIX = PhysicsFactory.createFixtureDef(0.0f, 0.0f, 0.0f);
         Rectangle ground = new Rectangle(0, CAMERA_HEIGHT - 15, CAMERA_WIDTH,
                 15, this.mEngine.getVertexBufferObjectManager());
-            ground.setColor(new Color(15, 50, 0));
+            ground.setColor(new org.andengine.util.color.Color(15, 50, 0));
         PhysicsFactory.createBoxBody(physicsWorld, ground, BodyType.StaticBody,
                 WALL_FIX);
         this.scene.attachChild(ground);
@@ -100,6 +126,9 @@ public class MainActivity extends BaseGameActivity {
         this.scene.attachChild(sPlayer);
         physicsWorld.registerPhysicsConnector(new PhysicsConnector(sPlayer,
                 body, true, false));
+
+        text = new Text(0, 0, mFont, "PIIRAKKA    PELI", this.getVertexBufferObjectManager());
+        this.scene.attachChild(text);
 
         pOnPopulateSceneCallback.onPopulateSceneFinished();
     }
