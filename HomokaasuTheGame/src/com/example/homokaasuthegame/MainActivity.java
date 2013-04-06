b1ffb4b1587bf8db4342bf90e2b60e35ed3e4c19
Olli Vanhoja <olli.vanhoja@gmail.com>
2013-04-07 01:39:18 +0300

    Fixed mainActivity

--- HomokaasuTheGame/src/com/example/homokaasuthegame/MainActivity.java
+++ HomokaasuTheGame/src/com/example/homokaasuthegame/MainActivity.java
@@ -263,9 +263,9 @@ public class MainActivity extends BaseGameActivity {
                 this.mEngine.getVertexBufferObjectManager());
         MainActivity.mainScene.attachChild(bg);
 
-        Enemy e = new Enemy(15, 10, 453f, 145f,
+        Enemy e = new Enemy(15, 10, 0, 453f, 145f,
                 enemyTextureRegion, this.getVertexBufferObjectManager());
-        
+
         mainScene.registerTouchArea(e);
 
         new Pie(CAMERA_WIDTH / 2, CAMERA_HEIGHT / 2, 400, 300,
