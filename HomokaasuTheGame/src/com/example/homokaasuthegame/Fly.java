622e534ba06717b7c970c04083fbc4773b15a502
Juha Lindqvist <juha.lindqvist@cs.helsinki.fi>
2013-04-07 03:27:45 +0300

    Fly updated

--- HomokaasuTheGame/src/com/example/homokaasuthegame/Fly.java
+++ HomokaasuTheGame/src/com/example/homokaasuthegame/Fly.java
@@ -1,19 +1,31 @@
 package com.example.homokaasuthegame;
 
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.region.ITextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
-import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.FixtureDef;
-
 public class Fly extends Enemy {
 	
+    private static BitmapTextureAtlas textureAtlas;
 	private static ITextureRegion textureRegion;
+	//size of the image
+	private static float HEIGHT = 108;
+	private static float WIDTH = 195;
+	private static int SPR_COLUMN = 2;
+	private static int SPR_ROWS = 1;
 	
-	public Fly(float pX, float pY, float rot, float pWidth, float pHeight,
-			ITextureRegion pTextureRegion,
-			VertexBufferObjectManager vertexBufferObjectManager) {
-		super(pX, pY, rot, pWidth, pHeight, pTextureRegion, vertexBufferObjectManager);
-		// TODO Auto-generated constructor stub
+	public Fly(float pX, float pY, VertexBufferObjectManager vertexBufferObjectManager) {
+		super(pX, pY, 0, WIDTH / 2, HEIGHT, textureRegion, vertexBufferObjectManager);
+	    this.animate(50);
+	}
+	
+	public static void init(MainActivity activity) {
+	    textureAtlas = new BitmapTextureAtlas(activity.getTextureManager(),
+	            (int) WIDTH, (int) HEIGHT, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+	    textureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(textureAtlas, activity.getAssets(),
+	            "fly_sprsheet.png", 0, 0, SPR_COLUMN, SPR_ROWS);
+	    textureAtlas.load();
 	}
 }
