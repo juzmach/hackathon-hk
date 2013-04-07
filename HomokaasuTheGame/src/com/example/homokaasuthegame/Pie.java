e0ef52c2c9ae50a4887044e6be92cc8c77a2cb5d
Kalle Viiri <kviiri@cs.helsinki.fi>
2013-04-07 15:21:58 +0300

    pie hp

--- HomokaasuTheGame/src/com/example/homokaasuthegame/Pie.java
+++ HomokaasuTheGame/src/com/example/homokaasuthegame/Pie.java
@@ -29,7 +29,7 @@ public class Pie extends AnimatedSprite {
 
     private static ITextureRegion pieTextureRegion;
 
-    private static final int maxHP = 5;
+    private static final int maxHP = 50;
     private int hp = maxHP;
 
     static void init(MainActivity activity) {
