3fcc130b943866a84c080a83517b69b692cc0e9b
Olli Vanhoja <olli.vanhoja@gmail.com>
2013-04-07 15:37:18 +0300

    Changed paussi texture

--- HomokaasuTheGame/src/com/example/homokaasuthegame/Paussi.java
+++ HomokaasuTheGame/src/com/example/homokaasuthegame/Paussi.java
@@ -1,21 +1,34 @@
 package com.example.homokaasuthegame;
 
-import org.andengine.entity.sprite.Sprite;
+import org.andengine.entity.sprite.AnimatedSprite;
 import org.andengine.input.touch.TouchEvent;
+import org.andengine.opengl.texture.TextureOptions;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlas;
+import org.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
 import org.andengine.opengl.texture.region.ITextureRegion;
+import org.andengine.opengl.texture.region.ITiledTextureRegion;
 import org.andengine.opengl.vbo.VertexBufferObjectManager;
 
-public class Paussi extends Sprite {
+public class Paussi extends AnimatedSprite {
+    private static int SPR_COLUMN  = 2;
+    private static int SPR_ROWS    = 1;
+
     private static ITextureRegion textureRegion;
     private boolean enabled = false;
 
     static void init(MainActivity activity) {
-        textureRegion = activity.loadTexture("pausebutton.png", 617, 617, 0, 0);
+        BitmapTextureAtlas textureAtlas;
+        textureAtlas = new BitmapTextureAtlas(activity.getTextureManager(),
+                1234, 617, TextureOptions.BILINEAR_PREMULTIPLYALPHA);
+        textureRegion = BitmapTextureAtlasTextureRegionFactory.createTiledFromAsset(
+                textureAtlas, activity.getAssets(),
+                "pausebutton_sprsheet.png", 0, 0, SPR_COLUMN, SPR_ROWS);
+        textureAtlas.load();
     }
 
     public Paussi(float pX, float pY,
             VertexBufferObjectManager pVertexBufferObjectManager) {
-        super(pX, pY, 617, 617, textureRegion, pVertexBufferObjectManager);
+        super(pX, pY, 617, 617, (ITiledTextureRegion) textureRegion, pVertexBufferObjectManager);
         this.setScale(0.1f);
         MainActivity.mainScene.attachChild(this);
         this.setZIndex(100);
@@ -34,10 +47,10 @@ public class Paussi extends Sprite {
         case TouchEvent.ACTION_UP:
             if (!enabled)
                 return false;
-
+            this.animate(new long[] {0}, new int[] {1}, 0);
             MainActivity.mainActivity.paussi();
             MainActivity.mainScene.unregisterTouchArea(this);
-            this.detachSelf();
+
             return false;
         }
         return true;
