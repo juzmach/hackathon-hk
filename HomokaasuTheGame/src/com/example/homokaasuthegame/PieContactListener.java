d974cd4050a42976e480d2a3cfba0ad58af8e536
Kalle Viiri <kviiri@cs.helsinki.fi>
2013-04-07 12:45:35 +0300

    timer frequency increased to one fire per second

--- HomokaasuTheGame/src/com/example/homokaasuthegame/PieContactListener.java
+++ HomokaasuTheGame/src/com/example/homokaasuthegame/PieContactListener.java
@@ -67,7 +67,7 @@ public class PieContactListener implements ContactListener
             return;
         timerRunning = true;
 
-        activity.getEngine().registerUpdateHandler(new TimerHandler(3.0f, new ITimerCallback()
+        activity.getEngine().registerUpdateHandler(new TimerHandler(1.0f, new ITimerCallback()
         {
             @Override
             public void onTimePassed(final TimerHandler pTimerHandler)
